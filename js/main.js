053cee40ed8bdaa5d97b6a92ab33e02824ee6fdd
rothlisbergerc <rothlisbergercheryl@gmail.com>
2021-06-15 12:24:10 -0700

    Initialized repository

---
+++ js/main.js
@@ -0,0 +1,100 @@
+var picker = datepicker("#due-date");
+picker.setMin(new Date());
+var ToDoItem = (function () {
+    function ToDoItem() {
+    }
+    return ToDoItem;
+}());
+window.onload = function () {
+    var addItem = document.getElementById("add");
+    addItem.onclick = main;
+};
+function main() {
+    if (isValid()) {
+        var item = getToDoItem();
+        displayToDoItem(item);
+    }
+}
+function isValid() {
+    var titleBox = document.getElementById("title");
+    var titleInput = titleBox.value;
+    if (titleInput == "") {
+        titleBox.nextElementSibling.innerHTML =
+            "Please enter a title";
+        return false;
+    }
+    if (titleInput != "") {
+        titleBox.nextElementSibling.innerHTML = "";
+        return true;
+    }
+}
+function resetErrorMessages() {
+    var allSpans = document.querySelectorAll("form span");
+    for (var i = 0; i < allSpans.length; i++) {
+        var currSpan = allSpans[i];
+        if (currSpan.hasAttribute("data-required")) {
+            currSpan.innerText = "*";
+        }
+        else {
+            currSpan.innerText = "";
+        }
+    }
+}
+function getToDoItem() {
+    var myItem = new ToDoItem();
+    var titleInput = getInput("title");
+    myItem.title = titleInput.value;
+    var dueDateInput = getInput("due-date");
+    myItem.dueDate = new Date(dueDateInput.value);
+    var isCompleted = getInput("is-complete");
+    myItem.isCompleted = isCompleted.checked;
+    return myItem;
+}
+function getInput(id) {
+    return document.getElementById(id);
+}
+function displayToDoItem(item) {
+    var itemText = document.createElement("h3");
+    itemText.innerText = item.title;
+    var itemDate = document.createElement("p");
+    itemDate.innerText = item.dueDate.toDateString();
+    var itemDiv = document.createElement("div");
+    itemDiv.onclick = markAsComplete;
+    itemDiv.classList.add("todo");
+    if (item.isCompleted) {
+        itemDiv.classList.add("completed");
+    }
+    itemDiv.appendChild(itemText);
+    itemDiv.appendChild(itemDate);
+    if (item.isCompleted) {
+        var completedToDos = document.getElementById("complete-items");
+        completedToDos.appendChild(itemDiv);
+    }
+    else {
+        var dueDateInput = getInput("due-date");
+        var dueDate = new Date(dueDateInput.value);
+        var today = new Date();
+        if ((addDays(today, 2) >= dueDate)) {
+            itemDiv.classList.add("major-priority");
+        }
+        if (dueDate > addDays(today, 2) && dueDate <= addDays(today, 5)) {
+            itemDiv.classList.add("medium-priority");
+        }
+        if (dueDate > addDays(today, 5)) {
+            itemDiv.classList.add("minor-priority");
+        }
+        var incompleteToDos = document.getElementById("incomplete-items");
+        incompleteToDos.appendChild(itemDiv);
+    }
+}
+function markAsComplete() {
+    var itemDiv = this;
+    itemDiv.classList.add("completed");
+    var completedItems = document.getElementById("complete-items");
+    completedItems.appendChild(itemDiv);
+}
+function addDays(date, days) {
+    var result = new Date(date);
+    result.setDate(result.getDate() + days);
+    return result;
+}
---
+++ ts/main.ts
@@ -0,0 +1,159 @@
+// @ts-ignore: Ignoring issue with js-datepicker lack of intellisense
+const picker = datepicker("#due-date");
+picker.setMin(new Date()); // Set to today's date
+
+class ToDoItem{
+    title:string;
+    dueDate:Date;
+    isCompleted:boolean;
+}
+
+window.onload = function(){
+    let addItem = document.getElementById("add");
+    addItem.onclick = main;
+
+}
+
+function main(){
+    if(isValid()){
+        let item = getToDoItem();
+        displayToDoItem(item);
+    }
+}
+
+/**
+ * Checks if textbox input is valid
+ */
+function isValid():boolean{    
+    let titleBox:HTMLInputElement = 
+        <HTMLInputElement>document.getElementById("title");
+    let titleInput:string = titleBox.value; 
+
+    if(titleInput == ""){        
+        titleBox.nextElementSibling.innerHTML = 
+            "Please enter a title";
+        return false;
+    }
+
+    if(titleInput != ""){        
+        titleBox.nextElementSibling.innerHTML = "";
+        return true;
+    }
+}
+
+/**
+ * Resets all the spans back to the default text
+ */
+function resetErrorMessages():void{
+    let allSpans = document.querySelectorAll("form span");
+    for(let i = 0; i < allSpans.length; i++){
+        let currSpan = <HTMLElement>allSpans[i];
+        if(currSpan.hasAttribute("data-required")){
+            currSpan.innerText = "*";
+        }
+        else{
+            currSpan.innerText = "";
+        }
+    }
+}
+
+/**
+ * Get all input off form and wrap in
+ * a ToDoItem object
+ */
+function getToDoItem():ToDoItem{
+    let myItem = new ToDoItem();
+    // get title
+    let titleInput = getInput("title");
+    myItem.title = titleInput.value;
+
+    // get due date
+    let dueDateInput = getInput("due-date");
+    myItem.dueDate = new Date(dueDateInput.value);
+
+    // get isCompleted
+    let isCompleted = getInput("is-complete");
+    myItem.isCompleted = isCompleted.checked;
+
+    return myItem;
+}
+
+function getInput(id):HTMLInputElement{
+    return <HTMLInputElement>document.getElementById(id);
+}
+
+/**
+ * Display given ToDoItem on the web page
+ */
+function displayToDoItem(item:ToDoItem):void{
+    // ex. <h3>Record JS Lecture</h3>
+    let itemText = document.createElement("h3");
+    itemText.innerText = item.title;
+
+    // ex. <p>June 1st 2020</p>
+    let itemDate = document.createElement("p");
+    itemDate.innerText = item.dueDate.toDateString();
+
+    // ex. <div class="todo completed"></div> or <div class="todo"></div>
+    let itemDiv = document.createElement("div");
+    itemDiv.onclick = markAsComplete;
+    itemDiv.classList.add("todo");
+
+    if(item.isCompleted){
+        itemDiv.classList.add("completed");
+    }
+
+    itemDiv.appendChild(itemText);
+    itemDiv.appendChild(itemDate);
+
+    if(item.isCompleted){
+        let completedToDos = document.getElementById("complete-items");
+        completedToDos.appendChild(itemDiv);
+    }
+    else{
+        let dueDateInput = getInput("due-date");
+        let dueDate = new Date(dueDateInput.value);
+        let today = new Date();
+
+        // if due date is within 3 days of current date(inclusive)
+        if((addDays(today, 2) >= dueDate )){ 
+            itemDiv.classList.add("major-priority");
+        }
+
+        // if due date is between 4 and 6 days after current date(inclusive)
+        if(dueDate > addDays(today, 2)  && dueDate <= addDays(today, 5) ){    
+            itemDiv.classList.add("medium-priority");
+
+        }
+        // if due date is 7 or more days after the current date(inclusive)
+        if(dueDate > addDays(today, 5)){
+            itemDiv.classList.add("minor-priority");
+
+        }
+
+        let incompleteToDos = document.getElementById("incomplete-items");
+        incompleteToDos.appendChild(itemDiv);
+
+    }
+}
+
+function markAsComplete(){
+    let itemDiv = <HTMLElement>this;
+    itemDiv.classList.add("completed");
+
+    let completedItems = document.getElementById("complete-items");
+    completedItems.appendChild(itemDiv);
+}
+
+/**
+ * Adds a specified number of days to a date object
+ * @param date 
+ * @param days 
+ * @returns 
+ */
+function addDays(date, days) {
+    let result = new Date(date);
+    result.setDate(result.getDate() + days);
+    return result;
+  }
+
